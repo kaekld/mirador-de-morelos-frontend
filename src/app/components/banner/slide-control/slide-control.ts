afdaed9fd85dd89de80eb39db07c2d8ab06a37c4
kaekld <kledmonroy@gmail.com>
2025-11-20 18:20:20 -0600

    banner carrousel ended

--- src/app/components/banner/slide-control/slide-control.ts
+++ src/app/components/banner/slide-control/slide-control.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-slide-control',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './slide-control.html',
   styles: ``,
 })
 export class SlideControl {
+  selectedBanner = input.required<number>();
+  clickBanner = output<number>()
 
+  handleClick(banner: number): void {
+    this.clickBanner.emit(banner)
+  }
 }
--- src/app/components/carousel/carousel.ts
+++ src/app/components/carousel/carousel.ts
@@ -1,7 +1,8 @@
-import { Component} from '@angular/core';
+import { Component, DestroyRef, inject, signal} from '@angular/core';
 import BannerContent from '../../models/Banner';
 import { Banner } from "../banner/banner";
 import { SlideControl } from "../banner/slide-control/slide-control";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-carousel',
@@ -10,7 +11,13 @@ import { SlideControl } from "../banner/slide-control/slide-control";
 })
 export class Carousel {
 
-  selectedBanner : number = 0;
+  selectedBanner = signal(0);
+  previousBanner = signal(0);
+
+  changerClass = true;
+
+  private timer: any;
+  private readonly intervalTime = 10000;
 
   banners: BannerContent[] =([
     {
@@ -25,7 +32,7 @@ export class Carousel {
       img: "https://sociedad-noticias.com/wp-content/uploads/2025/07/1-3.webp",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec risus  erat, facilisis eu lobortis id, interdum nec libero. In feugiat consequat rhoncus. Class aptent taciti sociosqu ad litora torquent per  conubia nostra, per inceptos himenaeos. Aenean sit amet lacinia eros.",
       buttonText: "Ver Noticias",
-      buttonClass: "text-2xl text-white bg-red-900 font-bold inline-block w-fit px-8 py-2 rounded-xl cursor-pointer hover:bg-red-950 duration-100"
+      buttonClass: "text-2xl text-white bg-blue-900 font-bold inline-block w-fit px-8 py-2 rounded-xl cursor-pointer hover:bg-red-950 duration-100"
     },
     {
       title: "NO TE PIERDAS DE LO MÁS INTERESANTE DE MORELOS",
@@ -35,5 +42,54 @@ export class Carousel {
       buttonClass: "text-2xl text-white bg-green-900 font-bold inline-block w-fit px-8 py-2 rounded-xl cursor-pointer hover:bg-red-950 duration-100"
     }
   ])
+
+
+  // * Funcion para iniciar el timer
+  private startTimer(): void {
+    if(this.timer) {
+      clearInterval(this.timer);
+    }
+
+    this.timer = setInterval(() => {
+      this.nextBanner()
+    }, this.intervalTime);
+  }
+
+  // * Iniciar del contador
+  constructor() {
+    this.startTimer();
+
+    const destroyRef = inject(DestroyRef);
+
+    destroyRef.onDestroy(()=>{
+      clearInterval(this.timer)
+    })
+  }
+
+  nextBanner(): void {
+    console.log(this.selectedBanner)
+
+    const total = this.banners.length;
+    this.previousBanner.set(this.selectedBanner());
+
+    // * Actualiza el índice del banner
+    this.selectedBanner.update(current => {
+        const nextValue = (current + 1) % total;
+        return nextValue;
+    });
+
+    this.changerClass = !this.changerClass;
+
+  }
+
+  clickedBanner(banner: number): void {
+    this.previousBanner.set(this.selectedBanner());
+
+    this.selectedBanner.set(banner);
+
+    this.changerClass = !this.changerClass;
+
+    this.startTimer();
+  }
 
 }
