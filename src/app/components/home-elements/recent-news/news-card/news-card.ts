939d43b238062b3e60b8a7d6a3bec9325a2b6e27
kaekld <kledmonroy@gmail.com>
2025-11-20 21:23:00 -0600

    new animations new card

--- src/app/components/home-elements/recent-news/news-card/news-card.ts
+++ src/app/components/home-elements/recent-news/news-card/news-card.ts
@@ -11,6 +11,7 @@ export class NewsCard {
   newInfo = signal<any>({
     title: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec risus  erat, facilisis eu lobortis id, interdum nec libero. In feugiat consequat rhoncus. Class aptent taciti sociosqu ad litora torquent per  conubianostra",
-    img: "https://rtvnoticiasmorelos.mx/wp-content/uploads/2025/11/Copia-de-contenido-noticias-ag-51.jpg"
+    img: "https://rtvnoticiasmorelos.mx/wp-content/uploads/2025/11/Copia-de-contenido-noticias-ag-51.jpg",
+    date: "22 de noviembre"
   })
 }
