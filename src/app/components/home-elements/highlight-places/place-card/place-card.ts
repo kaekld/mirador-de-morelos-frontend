1e48e9db9707faf7c0585d4ad6f4cfcb339729de
kaekld <kledmonroy@gmail.com>
2025-11-24 22:29:05 -0600

    fixed model

--- src/app/components/home-elements/highlight-places/place-card/place-card.ts
+++ src/app/components/home-elements/highlight-places/place-card/place-card.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { LucideAngularModule, MapPin, Phone, Instagram, Facebook } from 'lucide-angular';
 
 @Component({
   selector: 'app-place-card',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './place-card.html',
   styles: ``,
 })
@@ -14,4 +15,20 @@ export class PlaceCard {
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac  facilisis tellus.Maecenas pulvinar malesuada magna, vitae tristique  elit convallis sed. Aliquamtincidunt elit at lacus gravida luctus.  Quisque pretium vehicula vehicula. Nunc commodo sapien at erat mollis  eleifend. Nam efficitur risus vitae porta gravida. Donec tristique dolor vel mattis sodales.",
     img: "https://visitmorelos.mx/source/RESTAURANTES/KOOKABURRA/KOOKABURRA.jpg"
   })
+
+  activeModal: boolean = false;
+  fadeOut: boolean = false;
+
+  MapPin = MapPin;
+  Phone = Phone;
+  Instagram = Instagram;
+  Facebook = Facebook;
+
+  openModal(): void {
+    this.activeModal = true;
+    console.log(this.activeModal);
+  }
+  closeModal(): void {
+    this.activeModal = false;
+  }
 }
